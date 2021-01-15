64d4f0f76cda29e734610aa3994d417e4a08af6f
Ayush <58696933+Dragolite@users.noreply.github.com>
2021-01-16 01:25:59 +0530

    Delete agent.js

--- agent.js
+++
@@ -1,5 +0,0 @@
-const sidebar=document.querySelector('.sidebar');
-const sidebarToggler=document.querySelector('.sidebartoggler');
-sidebarToggler.addEventListener('click',()=>{
-   sidebar.classList.toggle('hidden')
-})
